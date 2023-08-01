5d3eec5b2c236944ee9653d67b842c3e573cf778
rahul1387 <rahultripathi7447@gmail.com>
2023-08-01 17:52:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//Ways to print in Javascript
+//console.log("Hello World");
+//alert("This is an alert");
+//document.write("This is an document write")
+
+// 2.Javascript console API
+console.log("Hello World", 6 + 4, "Another log")
+console.warn("This is warning")
+console.error("This is an error")
+
