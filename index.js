48d54da522748ea62084ce1365b0b3988f6abff7
rahul1387 <rahultripathi7447@gmail.com>
2023-08-02 18:06:33 +0530

    Javascript.

--- index.js
+++ index.js
@@ -3,8 +3,47 @@
 //alert("This is an alert");
 //document.write("This is an document write")
 
-// 2.Javascript console API
-console.log("Hello World", 6 + 4, "Another log")
-console.warn("This is warning")
-console.error("This is an error")
 
+// 2.Javascript console API
+// console.log("Hello World", 6 + 4, "Another log")
+// console.warn("This is warning")
+// console.error("This is an error")
+
+
+//3. Javascript variables
+// What are variables? - Container to store values.
+var number1 = 20;
+var number2 = 20;
+console.log(number1 + number2);
+
+// 4. Data Types in Javascript
+//Numbers
+var num1 = 10;
+var num2 = 15;
+
+//Strings
+var str1 = "Rhaul is a good boy";
+var str2 = 'Neeraj is a bad boy'; 
+
+//Objects
+console.log("This is Object:")
+var marks = {rahul: 99.99,neeraj: 88,sneha: 10,raj: 15,};
+console.log(marks);
+
+// Boolean
+var a = true;
+var b = false;
+console.log(a,b);
+
+// difference in undefined and null
+var und; //---> This is undefined means this variables has no value.
+var n = null; //---> This is null value means the variable has a null value.
+
+// There are two types of data types of dat a types in Javascript
+// 1. Primitive data types--> Undefine,Null,Boolean,Numbers
+// 2. reference data types---> Array and Object. 
+
+// Array
+console.log("This is Array:")
+var arr = [1,2,3,"Pizza",4];
+console.log(arr);
