6e6fed38a51dee6205a3e9ff5ba468d2ce52c01d
rahul1387 <rahultripathi7447@gmail.com>
2023-08-05 18:30:16 +0530

    Javascript.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@
 // What are variables? - Container to store values.
 var number1 = 20;
 var number2 = 20;
-console.log(number1 + number2);
+// console.log(number1 + number2);
 
 // 4. Data Types in Javascript
 //Numbers
@@ -26,14 +26,14 @@ var str1 = "Rhaul is a good boy";
 var str2 = 'Neeraj is a bad boy'; 
 
 //Objects
-console.log("This is Object:")
+// console.log("This is Object:")
 var marks = {rahul: 99.99,neeraj: 88,sneha: 10,raj: 15,};
-console.log(marks);
+// console.log(marks);
 
 // Boolean
 var a = true;
 var b = false;
-console.log(a,b);
+// console.log(a,b);
 
 // difference in undefined and null
 var und; //---> This is undefined means this variables has no value.
@@ -44,6 +44,61 @@ var n = null; //---> This is null value means the variable has a null value.
 // 2. reference data types---> Array and Object. 
 
 // Array
-console.log("This is Array:")
+// console.log("This is Array:")
 var arr = [1,2,3,"Pizza",4];
-console.log(arr);
+// console.log(arr);
+
+//Operators in Javascript
+//Arithmetic Operators
+var a = 100;
+var b = 10;
+// console.log("The value of a + b is: ", a+b);
+// console.log("The value of a - b is: ", a-b);
+// console.log("The value of a * b is: ", a*b);
+// console.log("The value of a / b is: ", a/b);
+
+//Assignment Operators
+var c = b;
+// c += 2;
+// c -= 2; // c = c - 2;
+// c *= 2;
+// c /= 2;
+// console.log(c);
+
+//Comparison Operators
+var x = 50;
+var y = 100;
+// console.log(x==y); 
+// console.log(x<=y); 
+// console.log(x>=y); 
+// console.log(x<y); 
+// console.log(x>y); 
+
+// Logical Operators
+
+// Logical AND(&&) operator
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && true);
+// console.log(false && false);
+
+//Logical OR(||) Operators
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false || true);
+// console.log(false || false);
+
+//Logical not(!) opeartor
+// console.log(!false);
+// console.log(!true);
+
+//Functions in Javascript
+// DRY --> Do not repeat yourself
+function avg(a,b)
+{
+    var c = (a + b)/2;
+    return c;
+}
+c1 = avg(4, 6);
+c2 = avg(10, 10);
+console.log(c1, c2);
