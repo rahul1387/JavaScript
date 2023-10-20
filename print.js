5c213675ef5eb3e22e85dcae3fd202df62c11faa
rahul1387 <rahultripathi7447@gmail.com>
2023-10-20 17:30:14 +0530

    Console Api in javascript.

---
+++ print.js
@@ -0,0 +1,8 @@
+// Ways to print in Javascript
+console.log("Hello world");
+alert("This is an alert"); // This will give alert on webpage
+
+// Javascript console API
+console.log("Hello World", 6 + 4, "Another log")
+console.warn("This is warning");
+console.error("This is an error");
