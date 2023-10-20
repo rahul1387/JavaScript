81fa599814f24c98aae5e54239b2ffeb54345c95
rahul1387 <rahultripathi7447@gmail.com>
2023-10-20 18:46:03 +0530

    Arrays in javascript

---
+++ array.js
@@ -0,0 +1,11 @@
+// Array ---> Which is used to store n number of item in list
+//Syntax 
+var arr = [12, 23, 36, 35, 45, "Rahul"]
+// We can also include string in array
+// console.log(arr);
+
+// Example 
+// 01 --> List of marks of five subjects and calculating percentage using array
+var marks = [80+98+63+95+12];
+var percentage = (marks/500)*100;
+console.log(percentage, '%');
