17505b8c67c721d41f147ba12009057b3045bf0d
rahul1387 <rahultripathi7447@gmail.com>
2023-10-20 17:51:51 +0530

    Variables and Data Types.

---
+++ variablesDataTypes.js
@@ -0,0 +1,36 @@
+// What are variables? -- Container which store some value
+var number1 = 20;
+var number2 = 10;
+console.log(number1 + number2);
+
+// Data Types in Javascript
+// Numbers
+var num1 = 15;
+var num2 = 11;
+
+// Strings ---> This data types can store strings 
+var str1 = "Rahul is a boy"; 
+
+// Objects ---> storing more data in one variable is kown as Object in javascript
+var obj = 
+{
+    name:"Rahul",
+    lastName:"Tripathi",
+    Gender:"Male"
+} 
+
+// Boolean --> Here the value can be true or false.
+var a = true; 
+var b = false;
+
+// Undefined ---> This means the value is not assign to var.So it will show undefined
+var und; 
+console.log(und);
+
+// Null ---> This means the value is asignned but it is null(Khalii)
+var nl = null;
+console.log(nl);
+
+//There are two types of data types
+    //1. Primitive data types : Undefined,Null,Numbers,Bollean
+    //2. Reference data types : Array, Objects  
