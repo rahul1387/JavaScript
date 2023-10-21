0a9facc1de6cd9df418a5f2365aed8cd1dc51333
rahul1387 <rahultripathi7447@gmail.com>
2023-10-21 15:45:27 +0530

    Functions in Javascript.

---
+++ function.js
@@ -0,0 +1,9 @@
+//Functions in Javascript
+// DRY --> Do not repeat yourself
+function avg(a, b) {
+    var c = (a + b) / 2;
+    return c;
+}
+c1 = avg(4, 6);
+c2 = avg(10, 10);
+console.log(c1, c2);
