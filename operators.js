7354d9dd0dbd3c5b27100464ad8f5c10558bc1c7
rahul1387 <rahultripathi7447@gmail.com>
2023-10-21 15:44:30 +0530

    Oparators in Javascript.

---
+++ operators.js
@@ -0,0 +1,43 @@
+// Operators in Javascript
+// Arithmetic Operators
+var a = 100;
+var b = 10;
+console.log("The value of a + b is: ", a+b);
+console.log("The value of a - b is: ", a-b);
+console.log("The value of a * b is: ", a*b);
+console.log("The value of a / b is: ", a/b);
+
+// Assignment Operators
+var c = b;
+c += 2;
+c -= 2; // c = c - 2;
+c *= 2;
+c /= 2;
+console.log(c);
+
+// Comparison Operators
+var x = 50;
+var y = 100;
+console.log(x==y); 
+console.log(x<=y); 
+console.log(x>=y); 
+console.log(x<y); 
+console.log(x>y); 
+
+// Logical Operators
+
+// Logical AND(&&) operator
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+// Logical OR(||) Operators
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+// Logical not(!) opeartor
+console.log(!false);
+console.log(!true);
