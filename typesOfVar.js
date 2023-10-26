39e091fba7934cb2fad592845dcb473522d6091d
rahul1387 <rahultripathi7447@gmail.com>
2023-10-26 16:59:12 +0530

    Types of variables in javascript.

---
+++ typesOfVar.js
@@ -0,0 +1,11 @@
+/* There are three types of variables in Javascript:
+                                                    1.var
+                                                    2.const
+                                                    3.let */    
+
+const num1 = 21; //---> We can't change the value of num1 because it is constant type variable
+// If we again assign num1 = 100; Then it will give error
+console.log(num1);
+
+let num2 = 15; //---> let can be only use in block container like objects
+console.log(num2);
