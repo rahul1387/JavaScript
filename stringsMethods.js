b67630854810180056d559d51e6ebe6798a3b572
rahul1387 <rahultripathi7447@gmail.com>
2023-10-26 17:18:46 +0530

    String methods in javascript.

---
+++ stringsMethods.js
@@ -0,0 +1,10 @@
+// Strings Methods in javascript
+
+let myStr = "Rahul is fasted reader in the world";
+console.log(myStr.length);
+console.log(myStr.indexOf("in"));
+console.log(myStr.lastIndexOf(" "));
+
+let m = myStr.replace("Rahul is", "John is also");
+console.log(myStr ,"but",m);
+//There are more methods including this all.
