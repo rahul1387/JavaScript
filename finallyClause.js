57ccace74cebfe5558d5910cf195dc44cd6d7507
rahul1387 <rahultripathi7447@gmail.com>
2023-11-21 19:26:19 +0530

    Finally clause in Javascript.

--- finallyClause.js
+++ finallyClause.js
@@ -5,10 +5,30 @@ try
 } catch (err) 
 {
     console.log("This is an error");
-    // console.log(err);
+    console.log(abcd);
 }
 finally
 {
     console.log("This is finall code");
 }
 // finally will run if there error in catch also.
+
+
+// if finally code is in function and if that function returns something,so before returning the finally code will execute.
+const y = ()=>{
+    try 
+    {
+        // console.log(program);
+        console.log("Program ran successfully");
+        return ;  
+    } catch (err) 
+    {
+        console.log("This is an error");
+        console.log(abcd);
+    }
+    finally
+    {
+        console.log("This code will before return");
+    }
+}
+y();
