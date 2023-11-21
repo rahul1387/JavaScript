e11ecbcb06d3ea027a10004177ec2a4bc19dcfc5
rahul1387 <rahultripathi7447@gmail.com>
2023-11-21 19:18:41 +0530

    .

---
+++ finallyClause.js
@@ -0,0 +1,14 @@
+try 
+{
+    // console.log(program);
+    console.log("Program ran successfully");    
+} catch (err) 
+{
+    console.log("This is an error");
+    // console.log(err);
+}
+finally
+{
+    console.log("This is finall code");
+}
+// finally will run if there error in catch also.
