d91f7a8e0eb391321171ba60a987014d6ad9483c
rahul1387 <rahultripathi7447@gmail.com>
2023-11-21 20:30:23 +0530

    Promise in javascript.

--- promise.js
+++ promise.js
@@ -1,24 +1,5 @@
-let p1 = new Promise((resolve,reject) =>{
-    console.log("Promise is pending");
-    setTimeout(()=>{
-        console.log("I am a promise and I am resolved");
-        resolve(true);
-    },5000);
-})
-let p2 = new Promise((resolve,reject) =>{
-    console.log("Promise is pending");
-    setTimeout(()=>{
-        // console.log("I am a promise and I am rejected");
-        reject(new Error("I'm an error"))
-    },5000);
+let promise = new Promise(function(resolve,reject){
+    resolve(13)
 })
 
-// To get the value 
-p1.then((value)=>{
-    console.log(value);
-})
-
-// To catch  errors
-p2.catch((error)=>{
-    console.log("The error was in p2");
-})
+console.log(promise);
